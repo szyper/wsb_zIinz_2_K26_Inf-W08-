9e0355f3e92d287cb65c21babd119ef7fa9f054b
szyper <zskgit@gmail.com>
2023-05-06 08:21:28 +0200

    register

--- 4_adminlte/scripts/register_user.php
+++ 4_adminlte/scripts/register_user.php
@@ -1,4 +1,22 @@
 <?php
-		echo "<pre>";
-			print_r($_POST);
-		echo "</pre>";
+	session_start();
+//		echo "<pre>";
+//			print_r($_POST);
+//		echo "</pre>";
+
+foreach ($_POST as $value){
+	if (empty($value)){
+		$_SESSION["error"] = "Wypełnij wszystkie pola!";
+		echo "<script>history.back()</script>";
+		exit();
+	}
+}
+
+$error = 0;
+
+if (!isset($_POST["terms"])){
+	$_SESSION["error"] = "Zaznacz regulamin!";
+	$error = 1;
+}
+
+echo "ok";
