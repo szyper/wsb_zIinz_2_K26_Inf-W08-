e2c47a3496a16d5dff93e33eadce51115328f534
szyper <zskgit@gmail.com>
2023-05-20 11:24:18 +0200

    register_user

--- 4_adminlte/scripts/login.php
+++ 4_adminlte/scripts/login.php
@@ -1,4 +1,5 @@
 <?php
+//session_destroy();
 session_start();
 
 if ($_SERVER["REQUEST_METHOD"] == "POST"){
@@ -26,24 +27,30 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){
 			$_SESSION["logged"]["firstName"] = $user["firstName"];
 			$_SESSION["logged"]["lastName"] = $user["firstName"];
 			$_SESSION["logged"]["logged_in"] = true;
-			//$_SESSION["logged"]["role"] = $user["role"];
-			session_regenerate_id();
+			$_SESSION["logged"]["role_id"] = $user["role_id"];
+			//session_regenerate_id();
 			$_SESSION["logged"]["session_id"] = session_id();
 			//echo session_id();
 
+
 			//czas sesji
 			//session.gc_maxlifetime=1440  => php.ini
-			$max_lifetime = ini_get('session.gc_maxlifetime');
+			//$max_lifetime = ini_get('session.gc_maxlifetime');
 			//echo $max_lifetime; //1440s => 24 minuty
 
 			//ustawienie czasu trwania sesji
 
-			ini_set('session.gc_maxlifetime', 1800); //30 minut
-			session_set_cookie_params(1800);
+			//ini_set('session.gc_maxlifetime', 1800); //30 minut
+			//session_set_cookie_params(1800);
 			/*
 			session_start();
 			exit();
 			*/
+
+			//setcookie("session_id_cookie", session_id());
+			setcookie("session_id_cookie", session_id(), time() + 3600, '/', 'localhost');
+			//setcookie("session_id_cookie", session_id(), time() + 3600);
+			echo "ciasteczko: ".$_COOKIE["session_id_cookie"]."<br>sesja: ".$_SESSION["logged"]["session_id"];
 
 			header("location: ../pages/logged.php");
 			exit();
